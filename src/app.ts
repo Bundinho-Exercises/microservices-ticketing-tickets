c6f0b1352af9aec7d1bb6bd041db9237cf2fd084
Toky Mahefa Ralala <toky@wau.co>
2020-12-31 18:29:33 +0300

    route tests

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,9 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import express from 'express';
-import { NotFoundError, errorHandler } from '@bdntickets/common';
+import { NotFoundError, errorHandler, currentUser } from '@bdntickets/common';
+
+import { createTicketRouter } from './routes/new';
 
 
 const app = express();
@@ -13,6 +15,10 @@ app.use(cookieSession({
     signed: false,
     secure: process.env.NODE_ENV !== 'test',
 }));
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all('*', async () => {
     throw new NotFoundError();
