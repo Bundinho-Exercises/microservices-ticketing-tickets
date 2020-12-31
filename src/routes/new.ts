ce3ea9b92752a4bd5dff4a868e575ad64257671c
Toky Mahefa Ralala <toky@wau.co>
2020-12-31 21:57:18 +0300

    new test

--- src/routes/new.ts
+++ src/routes/new.ts
@@ -2,6 +2,8 @@ import { requireAuth, validateRequest } from '@bdntickets/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 
+import { Ticket } from '../models/ticket';
+
 const router = express.Router();
 
 router.post('/api/tickets', requireAuth, [
@@ -10,10 +12,19 @@ router.post('/api/tickets', requireAuth, [
         .isEmpty()
         .withMessage('Title is required'),
     body('price')
-    .isFloat({ gt: 0 })
-    .withMessage('Price must be greater than 0'),
-], validateRequest, (req: Request, res: Response) => {
-    res.sendStatus(200);
+        .isFloat({ gt: 0 })
+        .withMessage('Price must be greater than 0'),
+], validateRequest, async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+    const ticket = Ticket.build({
+        title,
+        price,
+        userId: req.currentUser!.id,
+    });
+
+    await ticket.save();
+    res.status(201).send(ticket);
+
 });
 
 export { router as createTicketRouter };
